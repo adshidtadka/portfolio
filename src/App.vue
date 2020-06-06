b1c4a7157ac7423a7eec3a812906dc7f41ea86fe
adshidtadka <nextage900fx@gmail.com>
2020-06-06 15:17:43 +0900

    vueに変更

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <Loader></Loader>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Loader from "./components/Loader.vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Loader
+  }
+};
+</script>
+
+<style lang="scss">
+body {
+  font-family: "Archivo", sans-serif;
+  background-color: #343a40;
+  #app {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-size: 16px;
+    color: #2c3e50;
+    margin: 0 auto;
+    width: 80%;
+  }
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-navbar
+    class="header animate__animated animate__fadeInDown"
+    sticky
+    type="dark"
+    variant="transparent"
+  >
+    <b-navbar-brand class="header-title" href="#">adshidtadka</b-navbar-brand>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#">About</b-nav-item>
+        <b-nav-item href="#">Works</b-nav-item>
+        <b-nav-item href="#">Contact</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.header {
+  height: 80px;
+  font-size: 20px;
+  margin-bottom: 20px;
+
+  .header-title {
+    font-size: 26px;
+    font-weight: bold;
+    letter-spacing: 0.04em;
+  }
+}
+</style>
