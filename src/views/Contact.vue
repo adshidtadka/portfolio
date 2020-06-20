0e26e8585ffc50853abc06c746edb1ba80372db6
adshidtadka <nextage900fx@gmail.com>
2020-06-20 15:09:17 +0900

    コンタクトフォームからメールを送信する

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,19 +1,113 @@
 <template>
   <div id="contact">
-    Comming soon...
+    <b-form @submit.prevent="sendEmail" v-if="show">
+      <b-form-group
+        data-aos="fade-left"
+        data-aos-easing="ease-in-out"
+        data-aos-duration="1000"
+        id="input-group-2"
+        label="Name:"
+        label-for="input-2"
+      >
+        <b-form-input
+          name="from_name"
+          id="input-2"
+          v-model="form.name"
+          required
+          placeholder="Enter name"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group
+        data-aos="fade-right"
+        data-aos-easing="ease-in-out"
+        data-aos-duration="1000"
+        id="input-group-1"
+        label="Email:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          name="from_email"
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group
+        data-aos="fade-right"
+        data-aos-easing="ease-in-out"
+        data-aos-duration="1000"
+        id="input-group-3"
+        label="Message:"
+        label-for="input-3"
+      >
+        <b-form-textarea
+          name="message_html"
+          id="textarea-no-resize"
+          v-model="form.message"
+          required
+          placeholder="Enter message"
+          rows="10"
+          no-resize
+        >
+        </b-form-textarea>
+      </b-form-group>
+
+      <b-button
+        data-aos="fade-left"
+        data-aos-easing="ease-in-out"
+        data-aos-duration="1000"
+        type="submit"
+        variant="secondary"
+      >
+        Submit
+      </b-button>
+    </b-form>
   </div>
 </template>
 
 <script>
+import emailjs from "emailjs-com";
+
 export default {
   name: "Contact",
+  data() {
+    return {
+      form: {
+        email: "",
+        name: "",
+        message: "",
+      },
+      show: true,
+    };
+  },
+  created() {
+    console.log(process.env.VUE_APP_FOO);
+  },
+  methods: {
+    sendEmail(e) {
+      emailjs
+        .sendForm("contact_server", "template_xxxx", e.target, "user_xxxx")
+        .then(
+          (result) => {
+            console.log("SUCCESS!", result.status, result.text);
+          },
+          (error) => {
+            console.log("FAILED...", error);
+          }
+        );
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 #contact {
-  margin: 200px auto;
-  text-align: center;
+  margin: 50px auto;
   height: 100%;
 }
 </style>
