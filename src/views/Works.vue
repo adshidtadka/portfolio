ec4e1405af007ea1d67a67e5a7dff97cb05161f5
adshidtadka <nextage900fx@gmail.com>
2020-07-05 17:02:58 +0900

    modalを作成

--- src/views/Works.vue
+++ src/views/Works.vue
@@ -4,6 +4,7 @@
       <b-row class="row">
         <b-col cols="6">
           <b-card
+            v-b-modal.modal-blog
             data-aos="zoom-in"
             data-aos-easing="ease-in-out"
             data-aos-duration="1000"
@@ -14,31 +15,47 @@
             title="blog"
             class="card mb-2"
           >
-            <b-card-text>
+            <b-card-text class="text">
               WordPressで構築した<br />技術に関するブログ
             </b-card-text>
+            <b-modal centered id="modal-blog" title="blog" hide-footer>
+              サーバーとドメインを借りて自分で運用しています。<b-link
+                href="https://blog.adshidtadka.com"
+                target="_blank"
+                class="link"
+                >https://blog.adshidtadka.com</b-link
+              >
+            </b-modal>
           </b-card>
         </b-col>
-        <b-col cols="6">
+        <b-col cols="6" v-b-modal.modal-portfolio>
           <b-card
             data-aos="zoom-in"
             data-aos-easing="ease-in-out"
             data-aos-duration="1000"
             overlay
-            img-alt="Image"
             tag="article"
             title="portfolio"
-            class="mb-2"
+            class="card mb-2"
           >
             <b-card-text>
               Vue.jsで作ったこのポートフォリオサイト
             </b-card-text>
+            <b-modal
+              centered
+              id="modal-portfolio"
+              title="portfolio"
+              hide-footer
+            >
+              自分の技術力を示すためのものとして作りました。
+            </b-modal>
           </b-card>
         </b-col>
       </b-row>
       <b-row class="row">
         <b-col cols="6">
           <b-card
+            v-b-modal.modal-atsumeta
             data-aos="zoom-in"
             data-aos-easing="ease-in-out"
             data-aos-duration="1000"
@@ -47,15 +64,19 @@
             img-alt="Image"
             tag="article"
             title="atsumeta"
-            class="mb-2"
+            class="card mb-2"
           >
             <b-card-text>
               企業の技術ブログをまとめたキュレーションサイト
             </b-card-text>
+            <b-modal centered id="modal-atsumeta" title="atsumeta" hide-footer>
+              ハッカソンで賞を取ったアイディアを形にしたものです。開発中です。
+            </b-modal>
           </b-card>
         </b-col>
         <b-col cols="6">
           <b-card
+            v-b-modal.modal-simulator
             data-aos="zoom-in"
             data-aos-easing="ease-in-out"
             data-aos-duration="1000"
@@ -64,11 +85,30 @@
             img-alt="Image"
             tag="article"
             title="simulator"
-            class="mb-2"
+            class="card mb-2"
           >
             <b-card-text class="card-text">
               Python, C++で作ったサーバー割り当てシミュレーター
             </b-card-text>
+            <b-modal
+              centered
+              id="modal-simulator"
+              title="simulator"
+              hide-footer
+            >
+              <b-link
+                class="link"
+                href="https://www.jstage.jst.go.jp/article/transcom/advpub/0/advpub_2020EBP3006/_article"
+                target="_blank"
+              >
+                分散サーバー割り当て問題</b-link
+              >をアルゴリズムとソルバーで解いて実行時間を比較する<b-link
+                class="link"
+                href="https://github.com/adshidtadka/server-allocation"
+                target="_blank"
+                >シミュレーター</b-link
+              >を作りました。<br />
+            </b-modal>
           </b-card>
         </b-col>
       </b-row>
@@ -88,10 +128,10 @@ export default {
     margin: 20px 50px;
     .card {
       border: none;
-      max-width: 30rem;
-      .card-text {
-      }
     }
   }
+}
+.link {
+  color: #007bff;
 }
 </style>
